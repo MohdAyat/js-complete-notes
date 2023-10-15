10c88c9c1921b2fc8d0f027fd75b730663ad034e
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-15 12:23:22 +0530

    Update datatypes-summary.js
    just added lil bit more clarification

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,7 +1,7 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
-
+// Symbol--> it is used to make components unique below is an example u will its usage when u will work with react etc
 const score = 100
 const scoreValue = 100.3
 
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);// false
 
 // const bigNumber = 3456543576654356754n
 
