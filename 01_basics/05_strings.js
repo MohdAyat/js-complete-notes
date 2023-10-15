b86965fbde39d223a3b05dfb3b6f363cd8c92c3d
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-15 13:17:33 +0530

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,7 +3,8 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);// this is above written way to concatenate string
+instead use this way this is modern and very easy to write and unnderstand
 
 const gameName = new String('hitesh-hc-com')
 
@@ -19,17 +20,17 @@ console.log(gameName.indexOf('t'));
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 4)//slice is diff bcoz it allows neg index in which it start finding from backwards
 console.log(anotherString);
 
 const newStringOne = "   hitesh    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());//removes spaces
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'))//false
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-'));//[hitesh,hc,com]
