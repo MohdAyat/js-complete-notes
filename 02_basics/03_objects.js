b35fe7baeed2d5fb64eab32f36ff08b666995878
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-19 23:05:04 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,5 @@
-// singleton
-// Object.create
+// singleton object will be created when created using constructor
+// Object.create//this is how we create obj using constructor
 
 // object literals
 
@@ -9,7 +9,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1",//syntax for symbol data type
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -18,20 +18,20 @@ const JsUser = {
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser["email"])//another way to access object properties/methods
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySym])//syntax to access symbol data type which is member of object
 
 JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
+// Object.freeze(JsUser)//no propeties will be changed after this
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+JsUser.greetingTwo = function(){//creating func is just like vars
+    console.log(`Hello JS user, ${this.name}`);//O->Hello JS user, Hitesh
 }
 
 console.log(JsUser.greeting());
