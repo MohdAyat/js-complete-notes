5884e828d534a030c40b0d66ff8a2fe7298989df
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-19 19:19:13 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,23 +9,23 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]//spread all the elements and returns in a combined arr(spread operator)
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)//returns arr without subarrays but without missing any element(i.e.returned arr will contain all elements present at any depth in array)
 console.log(real_another_array);
 
 
 
 console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
+console.log(Array.from("Hitesh"))// takes an iterable object or arraylike
 console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));anything passed will be contverted in arr
