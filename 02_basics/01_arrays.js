2e2933b99cd5806e818789fc13e36373bdd8c815
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-19 17:45:00 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -12,28 +12,28 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9)// add element at the starting
+// myArr.shift()//remove starting element
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()//join the array in the string and return the string
 
 // console.log(myArr);
 // console.log( newArr);
 
 
-// slice, splice
+// slice, splice//both exclude the second parameter index
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)//it keeps the original array as it is
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3)//it also changes the original array
 console.log("C ", myArr);
 console.log(myn2);
