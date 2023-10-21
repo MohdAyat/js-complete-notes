5c3a810bf8293b13db0bae141a630f92f190752c
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-21 22:51:15 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() //constructor way to initiate obj(singleton obj)
+const tinderUser = {}//literal way to define obj
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -7,7 +7,7 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
-const regularUser = {
+const regularUser = {    //obj in obj in obj and so on any depth of nesting is possible
     email: "some@gmail.com",
     fullname: {
         userfullname: {
@@ -17,20 +17,20 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);//how to access obj property
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)//concat object syntax is (target(in which all sources will be going),source(which is going in target))
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2}//spread method to concat object
 // console.log(obj3);
 
 
-const users = [
+const users = [    //array of objs
     {
         id: 1,
         email: "h@gmail.com"
@@ -45,14 +45,14 @@ const users = [
     },
 ]
 
-users[1].email
+users[1].email//accessing particular objs property
 // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));//returns array of key value pair arrays O->[[id, "123abc"],[..],..] 
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));//asking if this obj has this property or not
 
 
 const course = {
