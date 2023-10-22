b980373d8ed202b6a9c8768010561272225816a1
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-22 15:14:31 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,21 +3,21 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+        console.log(`${this.username} , welcome to website`);//it will give current username
         console.log(this);
     }
 
 }
 
-// user.welcomeMessage()
+// user.welcomeMessage()//in output u will see hitesh as a username
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage()//in output u will see sam as a username
 
-// console.log(this);
+// console.log(this); this refers to current context but keep an eye on the scope here it is in the global scope so it won't give u above defined obj
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // it won't give u hitesh it works with objs
 // }
 
 // chai()
@@ -27,7 +27,7 @@ const user = {
 //     console.log(this.username);
 // }
 
-const chai =  () => {
+const chai =  () => {  //arrow method to define function very short method
     let username = "hitesh"
     console.log(this);
 }
@@ -43,7 +43,7 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) //syntax for objects 
 
 
 console.log(addTwo(3, 4))
