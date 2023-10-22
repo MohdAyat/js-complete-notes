5db321284820a335d43c2bf38d17523410073404
Mohd Ayat <124586432+MohdAyat@users.noreply.github.com>
2023-10-22 15:19:36 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,10 @@
-// Immediately Invoked Function Expressions (IIFE)
+// Immediately Invoked Function Expressions (IIFE) 
 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); //<= this ; is imp otherwise below written func will never be executed
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
